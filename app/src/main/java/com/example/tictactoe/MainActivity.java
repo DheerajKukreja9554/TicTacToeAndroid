e604c3c16dcc5d9125084acf40abcab8cc15d8f2
DheerajKukreja9554 <81379140+DheerajKukreja9554@users.noreply.github.com>
2021-09-05 16:55:14 +0530

    beautified the app and make mode button a imagebutton

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.tictactoe;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.example.tictactoe.gamesource.gameSource;
 
 public class MainActivity extends AppCompatActivity {
-    private int[] board;
-    private int activePlayer;
+
+    int[] board;
+    int activePlayer;
     TextView status;
     gameSource ob;
     int gamestate;
     boolean twoPlayer=true;
+
+    @SuppressLint("SetTextI18n")
     public void reinitialize()
     {
         board= new int[]{2, 2, 2, 2, 2, 2, 2, 2, 2};
@@ -27,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
             status.setText("Player's Turn");
         ob=new gameSource();
         gamestate=0;
-        ((ImageView)findViewById(R.id.imageView9)).setImageResource(0);
         ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
         ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
         ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
@@ -36,26 +39,29 @@ public class MainActivity extends AppCompatActivity {
         ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
         ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
         ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView9)).setImageResource(0);
 
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         status=findViewById(R.id.status);
+        ImageButton modeButton=findViewById(R.id.modeButton);
+        modeButton.setBackgroundResource(R.mipmap.twoplayer_foreground);
         reinitialize();
     }
+
     public void changeMode(View view){
-        Button mode=(Button)findViewById(R.id.modebutton);
-        if(twoPlayer)
-        {
-            twoPlayer=false;
-            mode.setText("VS AI");
+        ImageButton mode= (ImageButton) findViewById(R.id.modeButton);
+        if (twoPlayer) {
+            twoPlayer= false;
+            mode.setBackgroundResource(R.mipmap.computer_foreground);
         }
-        else
-        {
-            twoPlayer=true;
-            mode.setText("TWO PLAYER");
+        else{
+            twoPlayer= true;
+            mode.setBackgroundResource(R.mipmap.twoplayer_foreground);
         }
         reinitialize();
 
@@ -90,12 +96,12 @@ public class MainActivity extends AppCompatActivity {
                 ob.read(tapped / 3, tapped % 3, 'O');
                 img.setImageResource(R.drawable.o);
                 if(ob.isWon('O')){
-                    status.setText("\tPlayer2 Won \n Tap to Reset");
+                    status.setText("\tPlayer2 Won \nTap to Reset");
                     ++gamestate;
                 }
                 else if(!ob.couldWin('O'))
                 {
-                    status.setText("Game would end in a draw \n Tap to Reset");
+                    status.setText("Game would end in a draw \nTap to Reset");
                     ++gamestate;
                 }
                 else{
@@ -104,7 +110,6 @@ public class MainActivity extends AppCompatActivity {
                 }
                 board[tapped]=1;
             }
-//            Toast.makeText(this,ob.printboard(),Toast.LENGTH_SHORT).show();;
         }
 
     }
@@ -135,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else if(!ob.couldWin('X'))
                 {
-                    status.setText("Game would end in a draw \n Tap to Reset");
+                    status.setText("Game would end in a draw \nTap to Reset");
                     ++gamestate;
                 }
                 else {
@@ -151,25 +156,6 @@ public class MainActivity extends AppCompatActivity {
 
             int cturn=ob.compTurn();
             String cid= "imageView"+(cturn+1);
-//            int cid=ob.compTurn();
-//            if(cid==0&&board[cid]==2)
-//                img=findViewById(R.id.imageView1);
-//            else if(cid==1&&board[cid]==2)
-//                img=findViewById(R.id.imageView2);
-//            else if(cid==2&&board[cid]==2)
-//                img=findViewById(R.id.imageView3);
-//            else if(cid==3&&board[cid]==2)
-//                img=findViewById(R.id.imageView4);
-//            else if(cid==4&&board[cid]==2)
-//                img=findViewById(R.id.imageView5);
-//            else if(cid==5&&board[cid]==2)
-//                img=findViewById(R.id.imageView6);
-//            else if(cid==6&&board[cid]==2)
-//                img=findViewById(R.id.imageView7);
-//            else if(cid==7&&board[cid]==2)
-//                img=findViewById(R.id.imageView8);
-//            else if(cid==8&&board[cid]==2)
-//                img=findViewById(R.id.imageView9);
             img=findViewById(this.getResources().getIdentifier(cid,"id",getPackageName()));
             Log.d("com p turn",""+cid);
             img.setImageResource(R.drawable.o);
@@ -179,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
             }
             else if(!ob.couldWin('O'))
             {
-                status.setText("Game would end in a draw \n Tap to Reset");
+                status.setText("Game would end in a draw \nTap to Reset");
                 ++gamestate;
             }
             else {
